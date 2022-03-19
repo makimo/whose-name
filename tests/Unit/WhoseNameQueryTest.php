bc540383711feafb38733a5cdaa8514a21e5df83
Michał Moroz <michalmoroz@gmail.com>
2022-03-19 07:49:31 +0100

    Rename tests and rearrange them to be readable

--- tests/Unit/WhoseNameQueryTest.php
+++ tests/Unit/WhoseNameQueryTest.php
@@ -5,7 +5,7 @@ use Domain\WhoseName\IdentityQueryRepository;
 use Domain\WhoseName\Identity;
 
 
-test('given an Identity with two or more usernames, when asked what is the username in the other service, a correct username is returned', function () {
+test('Querying an existing user identity for a known service (e.g. GMail) returns username on that service', function () {
     $identity = new Identity([
         'jira' => 'test@makimo.pl',
         'slack' => 'U12345',
@@ -15,6 +15,7 @@ test('given an Identity with two or more usernames, when asked what is the usern
 
     $service = new QueryService($repo);
 
+    // Ask in one direction...
     $repo->shouldReceive('findByServiceAndUsername')
         ->with('jira', 'test@makimo.pl')
         ->andReturn($identity);
@@ -22,6 +23,7 @@ test('given an Identity with two or more usernames, when asked what is the usern
     $slackUsername = $service->whatIsTheNameOf('test@makimo.pl', 'jira', 'slack');
     expect($slackUsername)->toBeString()->toEqual('U12345');
 
+    // Or in the other one...
     $repo->shouldReceive('findByServiceAndUsername')
         ->with('slack', 'U12345')
         ->andReturn($identity);
@@ -32,50 +34,53 @@ test('given an Identity with two or more usernames, when asked what is the usern
 });
 
 
-test('given no Identity, QueryService returns null', function () {
-    $repo = Mockery::mock(IdentityQueryRepository::class);
-
-    $repo->shouldReceive('findByServiceAndUsername')
-        ->with('jira', 'test@makimo.pl')
-        ->andReturn(new Identity([]));
-    
-    $service = new QueryService($repo);
-    $result = $service->whatIsTheNameOf('test@makimo.pl', 'jira', 'slack');
-    expect($result)->toBeNull();
-});
-
-
-test('given an Identity with one username, asking about it confirms that it exist', function () {
+test('Querying an Identity for a not known service returns a null value', function () {
     $identity = new Identity([
         'jira' => 'test@makimo.pl',
     ]);
 
     $repo = Mockery::mock(IdentityQueryRepository::class);
 
+    $service = new QueryService($repo);
+
     $repo->shouldReceive('findByServiceAndUsername')
         ->with('jira', 'test@makimo.pl')
         ->andReturn($identity);
     
+    $result = $service->whatIsTheNameOf('test@makimo.pl', 'jira', 'slack');
+    expect($result)->toBeNull();
+});
+
+
+test('Querying a non-existent Identity for any service returns a null value', function () {
+    $identity = new Identity([]);
+    
+    $repo = Mockery::mock(IdentityQueryRepository::class);
+    
     $service = new QueryService($repo);
+
+    $repo->shouldReceive('findByServiceAndUsername')
+        ->with('jira', 'test@makimo.pl')
+        ->andReturn($identity);
+
+    $result = $service->whatIsTheNameOf('test@makimo.pl', 'jira', 'slack');
+    expect($result)->toBeNull();
+});
+
+
+test('Querying an existing Identity for the same known service returns the same username as provided', function () {
+    $identity = new Identity([
+        'jira' => 'test@makimo.pl',
+    ]);
+
+    $repo = Mockery::mock(IdentityQueryRepository::class);
+    
+    $service = new QueryService($repo);
+
+    $repo->shouldReceive('findByServiceAndUsername')
+        ->with('jira', 'test@makimo.pl')
+        ->andReturn($identity);
 
     $jiraUsername = $service->whatIsTheNameOf('test@makimo.pl', 'jira', 'jira');
     expect($jiraUsername)->toBeString()->toEqual('test@makimo.pl');
-});
-
-
-test('given an Identity, asking about nonexisting service returns null', function () {
-    $identity = new Identity([
-        'jira' => 'test@makimo.pl',
-    ]);
-
-    $repo = Mockery::mock(IdentityQueryRepository::class);
-
-    $repo->shouldReceive('findByServiceAndUsername')
-        ->with('jira', 'test@makimo.pl')
-        ->andReturn($identity);
-    
-    $service = new QueryService($repo);
-
-    $result = $service->whatIsTheNameOf('test@makimo.pl', 'jira', 'slack');
-    expect($result)->toBeNull();
 });
