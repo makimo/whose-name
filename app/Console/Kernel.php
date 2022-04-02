ca3a23c7d9d1fcffd79a2645fececbf5f56468d4
Michał Moroz <michalmoroz@gmail.com>
2022-04-02 21:20:54 +0200

    Add spresnac/laravel-create-user-cli to create users from console

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        \spresnac\createcliuser\CreateCliUserCommand::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
